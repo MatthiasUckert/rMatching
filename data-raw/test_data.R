67392a0b3cb0c59fc8e0309f4d42b55b19a789e2
Matthias Uckert <Matthias.Uckert@Outlook.com>
2022-10-05 18:45:03 +0200

    added revenue to test dataset

--- data-raw/test_data.R
+++ data-raw/test_data.R
@@ -16,7 +16,13 @@ table_target <- openxlsx::read.xlsx("data-raw/firms/orbis.xlsx") %>%
   dplyr::distinct(id, .keep_all = TRUE) %>%
   dplyr::mutate(dplyr::across(where(is.character), toupper)) %>%
   dplyr::mutate(iso3 = dplyr::if_else(dplyr::row_number() %in% c(20, 50, 70, 99), NA_character_, iso3)) %>%
-  dplyr::mutate(size = sample(c("small", "middle", "large"), nrow(.), replace = TRUE)) %>%
+  dplyr::mutate(
+    revenue = sample(100:400, nrow(.), replace = TRUE),
+    size = dplyr::case_when(
+      revenue <= 200 ~ "small",
+      revenue <= 300 ~ "middle",
+      revenue <= 400 ~ "large"
+    )) %>%
   dplyr::mutate(iso3 = dplyr::if_else(dplyr::row_number() %in% c(12:24, 1000:1050, 4000:4012), NA_character_, iso3))
 
 
@@ -29,7 +35,7 @@ table_source <- openxlsx::read.xlsx("data-raw/firms/compustat.xlsx") %>%
   dplyr::distinct(id, .keep_all = TRUE) %>%
   dplyr::mutate(dplyr::across(where(is.character), toupper)) %>%
   dplyr::mutate(iso3 = dplyr::if_else(dplyr::row_number() %in% c(21, 55, 71:74), NA_character_, iso3)) %>%
-  dplyr::left_join(table_target[, c("isin", "size")], by = "isin")
+  dplyr::left_join(table_target[, c("isin", "revenue", "size")], by = "isin")
 
 table_matches <- dplyr::inner_join(table_source, table_target, by = "isin", suffix = c("_s", "_t")) %>%
   dplyr::select(
